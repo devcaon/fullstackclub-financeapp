e2a8d60628fc51ca2a613c1dc4e805d462151522
Claudio Oliveira <dev.caon@gmail.com>
2024-03-17 21:11:47 -0300

    feat: add CreateUserUseCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user';
+
+export class CreateUserUseCase {
+  async execute(createUserParams) {
+    // TODO: check if email is already in use
+
+    // generate user id
+    const userID = uuidv4();
+
+    // encrypt password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(createUserParams.password, salt);
+
+
+    // insert user into database
+    const user = {
+      ...createUserParams,
+      id: userID,
+      password: hashedPassword,
+    }
+
+    // call repository
+    const postgresCreateUserRepository = new PostgresCreateUserRepository()
+
+    const createdUser = await postgresCreateUserRepository.execute(user)
+
+    return createdUser;
+  }
+}
