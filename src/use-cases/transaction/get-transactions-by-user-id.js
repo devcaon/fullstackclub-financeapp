0a08a53163e35242bf727884cf9f20735454ea59
Claudio Oliveira <dev.caon@gmail.com>
2024-03-27 22:24:13 -0300

    fix: ensure GetTransactionsByUserIdUseCase throws UserNotFoundError

---
+++ src/use-cases/transaction/get-transactions-by-user-id.js
@@ -0,0 +1,24 @@
+import { userNotFoundResponse } from "../../controllers/helpers"
+import { UserNotFoundError } from "../../errors/user"
+
+export class GetTransactionsByUserIdUseCase {
+
+  constructor(getTransactionByUserIdRepository, getUserByIdRepository) {
+    this.getTransactionByUserIdRepository = getTransactionByUserIdRepository
+    this.getUserByIdRepository = getUserByIdRepository
+  }
+
+  async execute(params) {
+
+    const user = await this.getUserByIdRepository.execute(params.userId)
+
+    if (!user) {
+      throw new UserNotFoundError(params.userId)
+    }
+
+    // Chamar o repository
+    const transactions = await this.getTransactionByUserIdRepository.execute(params.userId)
+
+    return transactions
+  }
+}
